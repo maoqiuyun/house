a34a8d67249569332e286c2aa6772e5931032863
maoqiuyun <maoqiuyun@ekohe.com>
2012-06-11 12:49:07 +0800

    git init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+House::Application.config.secret_token = 'fd4cd7946d89fc9f0c75c71d079c3e6fbca6ce729edf0cb5cbf7f5c8b167efea29cf23093c9bda4e54f5db721fd1c9ea9497ad58f225e1e0f84adea86f25d6eb'
